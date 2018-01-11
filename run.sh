b82c8f4277b6ae743a3487a8bba83f3d763841d3
Loris Friedel <loris.friedel@gmail.com>
2018-01-11 14:49:52 +0100

    Add exe (ubuntu), electroneum config and basic run script

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "-----------------------------------------"
+echo "Installing dependencies"
+echo "-----------------------------------------"
+
+sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+
+
+echo "-----------------------------------------"
+echo "Preparing environment"
+echo "-----------------------------------------"
+
+sudo sysctl -w vm.nr_hugepages=128
+
+
+echo "-----------------------------------------"
+echo "Running xmr stak cpu"
+echo "-----------------------------------------"
+
+CONFIG_XMR="config-electro.txt"
+echo "XMR stak cpu configuration: " $CONFIG_XMR
+
+screen -S unicorn sudo ./xmr-stak-cpu $CONFIG_XMR
