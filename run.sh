2755f58579d125b19df4c6eaf48a45ea4420da45
Loris Friedel <loris.friedel@gmail.com>
2018-01-13 17:28:40 +0100

    FIx and improve run.sh to allow miner config as first argument

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 CONFIG_XMR_STAK_CPU="config-electro.txt"
+if [ -z "$1" ] then
+    CONFIG_XMR_STAK_CPU="$1"
+fi
 
 echo "Installing dependencies"
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
@@ -9,8 +15,8 @@ echo ">> Preparing environment"
 sudo sysctl -w vm.nr_hugepages=128
 
 echo ">> Running xmr stak cpu"
-echo "> XMR stak cpu configuration: " $CONFIG_XMR_STAK_CPU
+echo "> XMR stak cpu configuration: " $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
 
-sed -i "s@RIG_ID@$HOSTNAME@" $CONFIG_XMR_STAK_CPU
+sed -i "s@RIG_ID@$HOSTNAME@" $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
 
-screen -S "STAK_$HOSTNAME" -dm ./xmr-stak-cpu $CONFIG_XMR_STAK_CPU
+screen -S "STAK_$HOSTNAME" -dm $SCRIPTPATH/xmr-stak-cpu $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
