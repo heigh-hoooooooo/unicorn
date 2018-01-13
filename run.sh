278adecfbd12c6af0c1ac3eaf9dfb55dafce00cb
Loris Friedel <loris.friedel@gmail.com>
2018-01-13 17:33:21 +0100

    Fix run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 CONFIG_XMR_STAK_CPU="config-electro.txt"
-if [[ -z "$1" ]] ; then
+if [[ ! -z "$1" ]] ; then
   CONFIG_XMR_STAK_CPU="$1"
 fi
 
