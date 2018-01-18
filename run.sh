e7290e6a8a14e33e1961e7aa1fdaba870e1d3d5a
Loris Friedel <loris.friedel@gmail.com>
2018-01-18 10:20:21 +0100

    Typo

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ echo ">> Running xmr stak cpu"
 nice -n -19 screen -S "STAK_$HOSTNAME" -dm nice -n -20 $SCRIPTPATH/xmr-stak-cpu $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
 
 STAKCPU_PROCESSES=$(pgrep xmr-stak-cpu)
-for $i in $STAKCPU_PROCESSES; do cpulimit -l 95 $i; done
+for i in $STAKCPU_PROCESSES; do cpulimit -l 95 $i; done
 
 sleep 2
 if pgrep -x "xmr-stak-cpu" > /dev/null
