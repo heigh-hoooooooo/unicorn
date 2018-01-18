11daeeadae3dc19c7fa1dbe97617974c52ca5765
Loris Friedel <loris.friedel@gmail.com>
2018-01-18 10:19:21 +0100

    Improve cpulimit setup

--- run.sh
+++ run.sh
@@ -19,11 +19,10 @@ sed -i "s@RIG_ID@$HOSTNAME@" $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
 
 echo ">> Running xmr stak cpu"
 
-nice -n -19 screen -S "STAK_$HOSTNAME" -dm $SCRIPTPATH/xmr-stak-cpu $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
+nice -n -19 screen -S "STAK_$HOSTNAME" -dm nice -n -20 $SCRIPTPATH/xmr-stak-cpu $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
 
-STAKCPU_PROCESS=$(pgrep -o -x xmr-stak-cpu)
-cpulimit -l 95 $STAKCPU_PROCESS
-renice -n -20 $STAKCPU_PROCESS
+STAKCPU_PROCESSES=$(pgrep xmr-stak-cpu)
+for $i in $STAKCPU_PROCESSES; do cpulimit -l 95 $i; done
 
 sleep 2
 if pgrep -x "xmr-stak-cpu" > /dev/null
