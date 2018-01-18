4a0fe6cbcd60c41f367b26de143b7d2ff6d3e2de
Loris Friedel <loris.friedel@gmail.com>
2018-01-18 10:24:03 +0100

    Add wait before renice processes

--- run.sh
+++ run.sh
@@ -21,11 +21,13 @@ echo ">> Running xmr stak cpu"
 
 nice -n -19 screen -S "STAK_$HOSTNAME" -dm $SCRIPTPATH/xmr-stak-cpu $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
 
+sleep 1
+
 STAKCPU_PROCESSES=$(pgrep xmr-stak-cpu)
 for i in $STAKCPU_PROCESSES; do cpulimit -l 95 $i; done
 for i in $STAKCPU_PROCESSES; do renice -n -20 $i; done
 
-sleep 2
+sleep 1
 if pgrep -x "xmr-stak-cpu" > /dev/null
 then
   echo ">> XMR stak cpu is now running!"
