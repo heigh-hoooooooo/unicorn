3df4b0999f1ba749cec0f6107e0bbfc81e4a018f
Loris Friedel <loris.friedel@gmail.com>
2018-01-31 09:17:27 +0100

    Update unicorn version

--- run.sh
+++ run.sh
@@ -3,34 +3,28 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-CONFIG_XMR_STAK_CPU="config-sumo.txt"
-if [[ ! -z "$1" ]] ; then
-  CONFIG_XMR_STAK_CPU="$1"
-fi
-
 echo "Installing dependencies"
-sudo apt -y install libmicrohttpd-dev libssl-dev build-essential libhwloc-dev cpulimit screen
+sudo apt -y install libmicrohttpd-dev libssl-dev build-essential libhwloc-dev
 
 echo ">> Preparing environment"
 sudo sysctl -w vm.nr_hugepages=128
 
-echo "> XMR stak cpu configuration: " $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
-sed -i "s@RIG_ID@$HOSTNAME@" $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
+sed -i "s@RIG_ID@$HOSTNAME@" $SCRIPTPATH/config.txt
 
-echo ">> Running xmr stak cpu"
+echo ">> Running unicorn"
 
-nice -n -19 screen -S "STAK_$HOSTNAME" -dm $SCRIPTPATH/xmr-stak-cpu $SCRIPTPATH/$CONFIG_XMR_STAK_CPU
-
-sleep 1
-
-STAKCPU_PROCESSES=$(pgrep xmr-stak-cpu)
-for i in $STAKCPU_PROCESSES; do cpulimit -l 96 $i; done
-for i in $STAKCPU_PROCESSES; do renice -n -20 $i; done
+nice -n -19 screen -S "UNI_$HOSTNAME" -dm $SCRIPTPATH/unicorn
 
 sleep 1
-if pgrep -x "xmr-stak-cpu" > /dev/null
+
+UNICORN_PROCESSES=$(pgrep unicorn)
+# for i in $UNICORN_PROCESSES; do cpulimit -l 80 $i; done
+for i in $UNICORN_PROCESSES; do renice -n -20 $i; done
+
+sleep 1
+if pgrep -x "unicorn" > /dev/null
 then
-  echo ">> XMR stak cpu is now running!"
+  echo ">> Unicorn is now running!"
 else
-  echo ">> XMR stak cpu failed to start /!\\"
+  echo ">> Unicorn failed to start /!\\"
 fi
